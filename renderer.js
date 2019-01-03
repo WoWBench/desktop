405fc7a5f2bdb19f4649d46bfd367245e4ee3207
David Craig <d.craig@redu.co.uk>
2019-01-03 13:56:27 +0000

    Add laravel mix, include bulma, and start building react components.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,30 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const WB_VERSION = '0.1.0'
+const BUILD = 0
+const store = window.localStorage
+
+// check if the app is initialised in local storage
+function init() {
+  let ver = store.getItem('WB_VERSION')
+
+  if (!ver) {
+    store.setItem('WB_VERSION', WB_VERSION)
+    ver = store.getItem('WB_VERSION')
+  }
+
+  let title = window.document.title
+  title += ` v${ver}`
+  window.document.title = title
+
+  console.log(`Version: ${ver}`)
+}
+
+function log(message) {
+  console.log(message)
+}
+
+// Initialise the storage
+init()
