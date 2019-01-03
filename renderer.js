6fb153710fbaf9f5897b31a767c7c95a66b5026b
David Craig <d.craig@redu.co.uk>
2019-01-03 14:23:17 +0000

    Load stylesheets using javascript.

--- renderer.js
+++ renderer.js
@@ -3,23 +3,43 @@
 // All of the Node.js APIs are available in this process.
 
 const WB_VERSION = '0.1.0'
-const BUILD = 0
+const WB_BUILD = 0
 const store = window.localStorage
+
+const addStylesheet = (stylesheet) => {
+  var head  = document.getElementsByTagName('head')[0];
+  var link  = document.createElement('link');
+  link.rel  = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'dist/' + stylesheet + '.css';
+  link.media = 'all';
+  head.appendChild(link);
+}
+
+function resetVersion() {
+  store.clear()
+}
 
 // check if the app is initialised in local storage
 function init() {
   let ver = store.getItem('WB_VERSION')
+  let b = store.getItem('WB_BUILD')
 
   if (!ver) {
     store.setItem('WB_VERSION', WB_VERSION)
+    store.setItem('WB_BUILD', WB_BUILD)
     ver = store.getItem('WB_VERSION')
+    b = store.getItem('WB_BUILD')
   }
 
   let title = window.document.title
   title += ` v${ver}`
   window.document.title = title
 
-  console.log(`Version: ${ver}`)
+  console.log(`Version: ${ver} [${b}]`)
+
+  addStylesheet('bulma')
+  addStylesheet('app')
 }
 
 function log(message) {
